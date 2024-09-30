8efafb193c5f8f5888b1f31e96213de91d840392
Stephan Model <stephan.model@outlook.de>
2024-09-30 15:58:22 +0200

    Solve react basics chall 2

---
+++ 24_react-bacics/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorldArticle />;
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Heading</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+        animi? Ab, repellat vitae! Quos tenetur sint repellat asperiores
+        nesciunt ea in sunt, optio repellendus cum incidunt, at adipisci quasi
+        necessitatibus?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+        animi? Ab, repellat vitae! Quos tenetur sint repellat asperiores
+        nesciunt ea in sunt, optio repellendus cum incidunt, at adipisci quasi
+        necessitatibus?
+      </p>
+    </article>
+  );
+}
