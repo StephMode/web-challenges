960903f9c7b839f64ce508e89e832caa5dbadb9f
Stephan Model <stephan.model@outlook.de>
2024-10-04 14:13:28 +0200

    Solve react state 2 chall 2

---
+++ 29_react-state-2/react-state-2_table-reservation/src/components/Counter.jsx
@@ -0,0 +1,27 @@
+// 💡 letztes was gefehlt hat war {} zwischen den props im param!
+
+export default function Counter({ onIncrement, onDecrement }) {
+  return (
+    <>
+      <h2>How many people would you like to visit us with?</h2>
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment people count"
+          onClick={onIncrement}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement people count"
+          onClick={onDecrement}
+        >
+          -
+        </button>
+      </div>
+    </>
+  );
+}
